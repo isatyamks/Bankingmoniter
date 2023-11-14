a359bbb15ce04a17d5839e3d0fe33315243fbcd2
Satyam Kumar Singh <isatyamks@gmail.com>
2023-11-14 15:53:12 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,33 +9,6 @@ private:
     string password;
     double balance;
 
-public:
-    BankingMonitorSystem(string uname, string pwd, double initial_balance)
-        : username(uname), password(pwd), balance(initial_balance) {}
-
-    bool authenticate(string entered_password) {
-        return entered_password == password;
-    }
-
-    void displayAccountInfo() {
-        cout << "Account Information for " << username << ":" << endl;
-        cout << "Balance: $" << fixed << setprecision(2) << balance << endl;
-    }
-
-    void deposit(double amount) {
-        balance += amount;
-        cout << "Deposit successful. New balance: $" << fixed << setprecision(2) << balance << endl;
-    }
-
-    void withdraw(double amount) {
-        if (amount <= balance) {
-            balance -= amount;
-            cout << "Withdrawal successful. New balance: $" << fixed << setprecision(2) << balance << endl;
-        } else {
-            cout << "Insufficient funds. Withdrawal failed." << endl;
-        }
-    }
-};
 
 int main() {
     BankingMonitorSystem userAccount("Satyam_kumar", "satyam9835.", 50000.0);
