b4d550f985979a30f342e59ae371ee117c03ff71
Satyam Kumar Singh <isatyamks@gmail.com>
2023-11-24 18:56:55 +0530

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,90 +1,114 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <vector>
 using namespace std;
+
+class Transaction {
+public:
+    string type; // "Deposit" or "Withdrawal"
+    double amount;
+    double balance_after_transaction;
+
+    Transaction(string t, double a, double b) : type(t), amount(a), balance_after_transaction(b) {}
+};
 
 class BankingMonitorSystem {
 private:
     string username;
     string password;
+    int accountNumber;
     double balance;
+    vector<Transaction> transactionHistory;
 
 public:
-    BankingMonitorSystem(string uname, string pwd, double initial_balance)
-        : username(uname), password(pwd), balance(initial_balance) {}
+    BankingMonitorSystem(string uname, string pwd, int accNumber, double initial_balance)
+        : username(uname), password(pwd), accountNumber(accNumber), balance(initial_balance) {}
 
     bool authenticate(string entered_password) {
         return entered_password == password;
     }
 
     void displayAccountInfo() {
-        cout << "Account Information for " << username << ":" << endl;
+        cout << "Account Information for " << username << " (Account Number: " << accountNumber << "):" << endl;
         cout << "Balance: Rs" << fixed << setprecision(2) << balance << endl;
     }
 
     void deposit(double amount) {
         balance += amount;
+        transactionHistory.push_back(Transaction("Deposit", amount, balance));
         cout << "Deposit successful. New balance: Rs" << fixed << setprecision(2) << balance << endl;
     }
 
     void withdraw(double amount) {
         if (amount <= balance) {
             balance -= amount;
+            transactionHistory.push_back(Transaction("Withdrawal", amount, balance));
             cout << "Withdrawal successful. New balance: Rs" << fixed << setprecision(2) << balance << endl;
         } else {
             cout << "Insufficient funds. Withdrawal failed." << endl;
         }
     }
+
+    void printTransactionHistory() {
+        cout << "Transaction History for " << username << " (Account Number: " << accountNumber << "):" << endl;
+        for (const Transaction& transaction : transactionHistory) {
+            cout << transaction.type << " of Rs" << fixed << setprecision(2) << transaction.amount;
+            cout << ", Balance after transaction: Rs" << fixed << setprecision(2) << transaction.balance_after_transaction << endl;
+        }
+    }
 };
 
 int main() {
-    BankingMonitorSystem userAccount("Satyam_kumar", "satyam9835.", 50000.0); 
+    int account_number;
+    cout << "Enter the account Number: ";
+    cin >> account_number;
 
+
+    BankingMonitorSystem userAccount("Satyam_kumar", "satyam9835.",9835100179, 50000.0);
+
+
+    
     string enteredPassword;
     cout << "Enter your password: ";
     cin >> enteredPassword;
 
-    if (userAccount.authenticate(enteredPassword)) 
-    {
+    if (userAccount.authenticate(enteredPassword)) {
         cout << "Authentication successful. Welcome!" << endl;
 
-        userAccount.displayAccountInfo();
-
-        int tempamount;
         int option;
-        cout<<"Enter\n(1)----> To display the amount\n(2)----> To deposit money\n(3)-----> To withdraw money";
-        cout<<endl;
 
-        cin>>option;
+        do {
+            cout << "Enter\n(1)----> To display the amount\n(2)----> To deposit money\n(3)-----> To withdraw money\n(4)-----> To print transaction history\n(0)-----> To exit" << endl;
+            cin >> option;
 
-        if(option==1){
-            userAccount.displayAccountInfo();
-            cout<<"\n";
-            cout<<"Thanks :)"<<endl;
-        }
-        else if (option==2){
-            cout<<"Enter the Amount you want to deposit\n";
-            cin>>tempamount;
-            userAccount.deposit(tempamount);
-            cout<<"\n";
-            userAccount.displayAccountInfo();
+            switch (option) {
+                case 1:
+                    userAccount.displayAccountInfo();
+                    break;
+                case 2:
+                    double tempamount;
+                    cout << "Enter the Amount you want to deposit: ";
+                    cin >> tempamount;
+                    userAccount.deposit(tempamount);
+                    break;
+                case 3:
+                    cout << "Enter the Amount You want to withdraw: ";
+                    cin >> tempamount;
+                    userAccount.withdraw(tempamount);
+                    break;
+                case 4:
+                    userAccount.printTransactionHistory();
+                    break;
+                case 0:
+                    cout << "Exiting program." << endl;
+                    break;
+                default:
+                    cout << "Error: Invalid option." << endl;
+            }
 
-        }
-        else if(option==3){
-            cout<<"Enter the Amount You want to withdraw\n";
-            cin>>tempamount;
-            userAccount.withdraw(tempamount);
-            cout<<"\n";
-            userAccount.displayAccountInfo();
-
-        }
-        else{
-            cout<<" Error 404"<<endl;
-        }
-    } 
-    
-    else
-    {
+        } while (option != 0);
+    } else {
         cout << "Authentication failed. Exiting program." << endl;
     }
 
